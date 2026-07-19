c5ff3de178961169d93acef23336dbe826600169
Octa <octavianwahyus@outlook.com>
2026-07-19 18:53:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-  page_title = "East Java Climatology Analysis"
-  layout = wide
+  page_title = "East Java Climatology Analysis",
+  layout = wide,
 )
 
