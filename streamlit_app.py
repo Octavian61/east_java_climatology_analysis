c527357f2a143812fe4d20f1a7c58e83075d8715
Octa <octavianwahyus@outlook.com>
2026-07-19 19:00:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
   page_title = "East Java Climatology Analysis",
@@ -8,3 +9,9 @@ st.set_page_config(
 st.title("East Java Climatology Analysis Dashboard")
 st.markdown("Analisis klimatologi kawasan Jawa Timur dari data NASA dalam rentang 2015-2025")
 st.write("---")
+
+@st.cache_data
+def load_data():
+  df = pd.read_csv("climatology_daily_2015_2025.csv")
+  return df
+
