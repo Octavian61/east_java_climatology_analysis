ce1bb4900435ee04f36f885fd963c565363da455
Octa <octavianwahyus@outlook.com>
2026-07-20 17:34:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,16 @@ tab1, tab2 = st.tabs(["Variabilitas Suhu"], ["Analisis Hujan Besar"])
 
 with tab1:
   df_monthly = df_filtered['T2M'].resample('RE').mean().to_frame(name = "T2M_mean")
+  df_monthly['T2M_rolling_12] = df_monthly['T2M_mean'].rolling(window = 12, center = True).mean()
+  fig, ax = plt.subplots(figsize=(10, 4))
+  ax.plot(df_monthly.index, df_monthly['T2M_mean'], label='Suhu Bulanan Asli', color='gray', alpha=0.4)
+  ax.plot(df_monthly.index, df_monthly['T2M_rolling_12'], label='Tren (Rolling Avg 12 Bulan)', color='red', linewidth=2)
+  ax.set_ylabel("Suhu (°C)")
+  ax.legend()
+  ax.grid(True, linestyle='--', alpha=0.5)
+  st.pyplot(fig)
+
+with tab2:
+  st.markdown("#### Top 5 Hari Hujan Paling Besar")
+  top_5_hujan = df_filtered.nlargest(5, "PRECTOTCORR")[['PRECTOTCORR'], ['T2M'], ['RH2M']
+  st.dataframe(top_5_hujan)
